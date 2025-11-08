be741feed485f7012b9dffccb7468145639686e9
chyVacheck <dmytro.shah.work@gmail.com>
2025-11-08 15:41:06 +0100

    feat: добавить подключение к базе данных

--- src/index.ts
+++ src/index.ts
@@ -8,12 +8,19 @@
  */
 import { SERVER_CONFIG } from '@config';
 import { Evo } from '@core/app';
+import { MongoDatabase } from '@core/database';
 import { ApiRouter } from '@modules/api';
+import { UserRepository } from '@modules/user/User.repository';
+import { UserService } from '@modules/user/User.service';
 
 /**
  * Создаем экземпляр приложения Evo.
  */
 const evo = new Evo('/');
+
+const mongoDb = new MongoDatabase();
+
+await mongoDb.connect();
 
 // Старт
 evo.listen({ port: SERVER_CONFIG.PORT });
